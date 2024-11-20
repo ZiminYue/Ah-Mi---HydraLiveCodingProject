15e652d70337d2db9f6acb78d9fc7974f4d0348d
ZiminYue <1824251299@qq.com>
2024-11-20 20:14:13 +0000

    Update ZiminYueCode-Testing.js

--- Codes/ZiminYueCode-Testing.js
+++ Codes/ZiminYueCode-Testing.js
@@ -72,6 +72,19 @@ osc(3, 0.01, 0.4)
 .out(o0)
 
 
+// licensed with CC BY-NC-SA 4.0 https://creativecommons.org/licenses/by-nc-sa/4.0/
+//Flor de Fuego
+
+shape(200,0.5,1.5)
+.scale(0.5,0.5)
+.color([0.5,2].smooth(1),0.3,0)
+.repeat(2,2)
+.modulateScale(osc(3,0.5),-0.6)
+.add(o0,0.5)
+.scale(0.9)
+.out()
+
+
 ////I dunno why it becomes like raining gold?!
 src(o0)
  .saturate(1)
