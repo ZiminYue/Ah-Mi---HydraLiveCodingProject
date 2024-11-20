b3611248fda6ec873827e073954ef86753c7d758
ZiminYue <1824251299@qq.com>
2024-11-20 01:03:06 +0000

    Update ZiminYueCode-Testing.js

--- Codes/ZiminYueCode-Testing.js
+++ Codes/ZiminYueCode-Testing.js
@@ -55,6 +55,21 @@ shape([4,5,6].fast(0.1).smooth(1),0.000001,[0.2,0.7].smooth(1))
 .modulate(voronoi(10,2,2))
 .out()
 
+// licensed with CC BY-NC-SA 4.0 https://creativecommons.org/licenses/by-nc-sa/4.0/
+//CNDSD
+//http://malitzincortes.net/
+// sand spirals
+
+osc(3, 0.01, 0.4)
+.color(1.2,1.2,1.3)
+.saturate(0.4)
+.modulateRepeat(osc(2),1, 2, 4, 3)
+.modulateKaleid(osc(12,0.05,0),1)
+.luma (0.4)
+.rotate(4, 0.1,0)
+.modulate(o0, () => mouse.y *0.0002 )
+.scale(1).diff(o1)
+.out(o0)
 
 
 ////I dunno why it becomes like raining gold?!
