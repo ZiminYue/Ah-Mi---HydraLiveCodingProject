c8937fcb21b5a79852f93e3d5438f7ff3cabc677
Rittaaa1 <linxx027@163.com>
2024-11-20 20:17:42 +0000

    Update Xinxinlin.js

--- Codes/Xinxinlin.js
+++ Codes/Xinxinlin.js
@@ -1,1 +1,14 @@
-Xinxin lin codes start here!
+//Xinxin lin codes start here!
+//湖面泡泡水
+// licensed with CC BY-NC-SA 4.0 https://creativecommons.org/licenses/by-nc-sa/4.0/
+// 3.0
+// by ΔNDR0M3DΔ
+// https://www.instagram.com/androm3_da/
+
+
+noise(3,0.3,3).thresh(0.3,0.03).diff(o3,0.3).out(o1)
+gradient([0.3,0.3,3]).diff(o0).blend(o1).out(o3)
+voronoi(33,3,30).rotate(3,0.3,0).modulateScale(o2,0.3).color(-3,3,0).brightness(3).out(o0)
+shape(30,0.3,1).invert(({time})=>Math.sin(time)*3).out(o2)
+
+render(o3)
