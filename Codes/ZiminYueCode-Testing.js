04aae3d1c62128e0dee62c052d5c7ff7ae057a45
ZiminYue <1824251299@qq.com>
2024-11-21 10:43:22 +0000

    Update ZiminYueCode-Testing.js 欣欣你来调这个漂亮大丛林

--- Codes/ZiminYueCode-Testing.js
+++ Codes/ZiminYueCode-Testing.js
@@ -96,6 +96,28 @@ voronoi(10,3,5)
 shape(3,0.1,1).modulate(noise(7),()=>a.fft[0]).rotate( () => time%360*2 ).brightness( () => Math.sin(time*3) ).color(5,1,0.01).out(o0)
 
 
+// licensed with CC BY-NC-SA 4.0 https://creativecommons.org/licenses/by-nc-sa/4.0/
+//tropical juice
+//by Ritchse
+//instagram.com/ritchse
+ 
+voronoi(2,0.3,0.2).shift(0.5)
+.modulatePixelate(voronoi(4,0.2),32,2)
+.scale(()=>1+(Math.sin(time*2.5)*0.05))
+.diff(voronoi(3).shift(0.6))
+.diff(osc(2,0.15,1.1).rotate())
+.brightness(0.1).contrast(1.2).saturate(1.2)
+	.out()
+speed = 0.8
+
+
+
+
+
+
+
+
+
 ////I dunno why it becomes like raining gold?!
 src(o0)
  .saturate(1)
